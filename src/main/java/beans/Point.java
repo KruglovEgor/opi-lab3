3ce650d7d42a28b8523136f515554d6b568b80a1
KruglovEgor <367327@edu.itmo.ru>
2024-05-28 23:00:58 +0300

    Commit changes from 05.28.2024 11:00:58 PM

--- src/main/java/beans/Point.java
+++ src/main/java/beans/Point.java
@@ -8,7 +8,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
-//ff
+
 @ManagedBean(name = "point")
 @SessionScoped
 @NoArgsConstructor
